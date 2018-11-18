56a5ceeaf9bec67d5f4375ae7f7331d90aced56d
Abid H <abidhussain@pursuit.org>
2018-11-18 10:57:06 -0500

    incomplete

---
+++ script.js
@@ -0,0 +1,19 @@
+// const testNum = num => {
+//   return new Promise(function(resolve, reject) {
+//     resolve(num > 10);
+//   }).then(ans => {
+//     console.log(ans);
+//   });
+// };
+// testNum(12);
+
+// const whisperShout = str => {
+//   console.log(str);
+//
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(str.toUpperCase())
+//     }, 3000)
+//   }).then( => {
+//   })
+// };
